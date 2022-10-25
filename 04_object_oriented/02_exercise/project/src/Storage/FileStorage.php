10a2fffad31fe47c5e153f0b8a26cf1eb9638c85
lewi9 <lewinski@student.agh.edu.pl>
2022-10-25 15:08:51 +0200

    loadAll().

--- 04_object_oriented/02_exercise/project/src/Storage/FileStorage.php
+++ 04_object_oriented/02_exercise/project/src/Storage/FileStorage.php
@@ -3,16 +3,22 @@
 namespace Storage;
 
 use Concept\Distinguishable;
+use Config\Directory;
 
 class FileStorage implements Storage
 {
     public function store(Distinguishable $distinguishable) : void
     {
         $data = serialize($distinguishable);
-        $file = $distinguishable.key();
+        $file = $distinguishable->key();
+        file_put_contents(Directory::storage() . "/" . $file, $data);
     }
     public function loadAll() : iterable
     {
-        return;
+        $dir = scandir(Directory::storage());
+        for($i = 2; $i<count($dir); $i++)
+        {
+            yield unserialize(file_get_contents(Directory::Storage() .$dir[$i]));
+        }
     }
 }
