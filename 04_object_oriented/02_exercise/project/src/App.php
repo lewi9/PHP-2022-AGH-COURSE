9a602c9b3820a378a5d325543c8eab38dd8fff46
lewi9 <lewinski@student.agh.edu.pl>
2022-10-25 15:39:09 +0200

    App body.

--- 04_object_oriented/02_exercise/project/src/App.php
+++ 04_object_oriented/02_exercise/project/src/App.php
@@ -6,6 +6,19 @@ class App
 {
     public function run(): void
     {
+        $buttons = array();
+        $links = array();
+        $storage = new \Storage\FileStorage();
+        for($i = 0; $i<5; $i++ )
+        {
+            $buttons[] = new \Widget\Button($i);
+            $links[] = new \Widget\Link($i);
+        }
+        for($i = 0; $i<5; $i++ )
+        {
+            $storage->store($buttons[$i]);
+            $storage->store($links[$i]);
+        }
 
     }
     private function render (Widget $widget): void
