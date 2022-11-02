e3a896ee1fff4c25f23e7167939d7dd2adaeeaad
lewi9 <lewinski@student.agh.edu.pl>
2022-11-02 14:58:18 +0100

    Initial commit

--- skrypt.sh
+++ skrypt.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 cd ../php_2022_g2
-cp "$1/" "../php_2022_g2_adam_lewinski/$1/" -r
+cp "$1/" "../php_2022_adam_lewinski/$1/" -r
 
 cd ../php_2022_g2
-cp "bitbucket-pipelines.yml" "../php_2022_g2_adam_lewinski/bitbucket-pipelines.yml"
+cp "bitbucket-pipelines.yml" "../php_2022_adam_lewinski/bitbucket-pipelines.yml"
 
 cd ../php_2022_g2
-cp "index.ipynb" "../php_2022_g2_adam_lewinski/index.ipynb"
+cp "index.ipynb" "../php_2022_adam_lewinski/index.ipynb"
+
+cd ../php_2022_g2
+cp ".gitignore" "../php_2022_adam_lewinski/.gitignore"
