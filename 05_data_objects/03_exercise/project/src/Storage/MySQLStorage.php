942ac282a79132d10dbfd9eba296f0cc0746d71d
lewi9 <lewinski@student.agh.edu.pl>
2022-11-02 15:02:01 +0100

    Body of classes

---
+++ 05_data_objects/03_exercise/project/src/Storage/MySQLStorage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Storage;
+
+use Concept\Distinguishable;
+
+class MySQLStorage implements Storage
+{
+
+    public function store(Distinguishable $distinguishable): void
+    {
+        // TODO: Implement store() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function loadAll(): array
+    {
+        // TODO: Implement loadAll() method.
+    }
+}
