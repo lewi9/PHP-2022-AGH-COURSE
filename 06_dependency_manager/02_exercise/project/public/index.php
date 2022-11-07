b95b35828e1d1affe48963bf50558a8328826425
lewi9 <lewinski@student.agh.edu.pl>
2022-11-07 23:26:37 +0100

    Predis autoload problem - PHP stan.

--- 06_dependency_manager/02_exercise/project/public/index.php
+++ 06_dependency_manager/02_exercise/project/public/index.php
@@ -8,7 +8,8 @@ ini_set("display_errors", "On");
 // Autoload
 
 require("../vendor/autoload.php");
-
+require "../vendor/predis/predis/autoload.php";
+Predis\Autoloader::register();
 // Setup directory config
 
 Config\Directory::set("../");
