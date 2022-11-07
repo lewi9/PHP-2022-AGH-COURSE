1fef54c645cf05b4428edc106a73a951609d3853
lewi9 <lewinski@student.agh.edu.pl>
2022-11-07 22:30:49 +0100

    Composer installed.

---
+++ 06_dependency_manager/02_exercise/project/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lewi9/project',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f94a0ff11074e4a3e82f0d4891cad2377fd4f0b7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lewi9/project' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f94a0ff11074e4a3e82f0d4891cad2377fd4f0b7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
