aad08923156ae6cfef1d6ef2756897fb91e9c6ee
lewi9 <lewinski@student.agh.edu.pl>
2022-11-20 15:26:29 +0100

    String/Editor

--- 07_testing/02_exercise/project/src/String/Editor.php
+++ 07_testing/02_exercise/project/src/String/Editor.php
@@ -16,10 +16,37 @@ class Editor
         return $this->string;
     }
 
-    public function replace(string $search, string $replace): void
+    public function replace(string $search, string $replace): Editor
     {
-        str_replace($search, $replace, $this->string);
+        $this->string = str_replace($search, $replace, $this->string);
+        return $this;
     }
 
-    public function lower
+    public function lower(): Editor
+    {
+        $this->string = strtolower($this->string);
+        return $this;
+    }
+
+    public function upper(): Editor
+    {
+        $this->string = strtoupper($this->string);
+        return $this;
+    }
+
+    public function censor(string $replace): Editor
+    {
+        $count = strlen($replace);
+        return $this->replace($replace, "*")->repeat("*", $count);
+    }
+
+    public function repeat(string $repeat, int $count): Editor
+    {
+        return $this->replace($repeat, str_repeat($repeat, $count));
+    }
+
+    public function remove(string $remove): Editor
+    {
+        return $this->replace("$remove", "");
+    }
 }
