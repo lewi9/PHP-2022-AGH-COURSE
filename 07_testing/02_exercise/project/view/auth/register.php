27395e094c3e991181f1f05e185e6ee08bf45205
lewi9 <lewinski@student.agh.edu.pl>
2022-11-20 20:11:56 +0100

    Register form.

--- 07_testing/02_exercise/project/view/auth/register.php
+++ 07_testing/02_exercise/project/view/auth/register.php
@@ -1,1 +1,16 @@
 <h2>Register</h2>
+<form>
+    <label for="id">Id:</label>
+    <input type="text" id="id" name="id"><br><br>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name"><br><br>
+    <label for="surname">Surname:</label>
+    <input type="text" id="surname" name="surname"><br><br>
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email"><br><br>
+    <label for="password">Password:</label>
+    <input type="text" id="password" name="password"><br><br>
+    <label for="password_confirmation">Password confirmation:</label>
+    <input type="text" id="password_confirmation" name=password_confirmation><br><br>
+    <input type="submit" value="Create">
+</form>
