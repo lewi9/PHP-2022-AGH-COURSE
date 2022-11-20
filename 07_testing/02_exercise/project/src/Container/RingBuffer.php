fba1439d402b452e0425cf1527a1937876a08f72
lewi9 <lewinski@student.agh.edu.pl>
2022-11-20 16:33:41 +0100

    RingBuffer push

--- 07_testing/02_exercise/project/src/Container/RingBuffer.php
+++ 07_testing/02_exercise/project/src/Container/RingBuffer.php
@@ -8,6 +8,8 @@ class RingBuffer
     private int $head;
     private int $tail;
     private int $size;
+    /** @var array<int> */
+    private array $arr;
 
     public function __construct(int $capacity)
     {
@@ -15,6 +17,7 @@ class RingBuffer
         $this->tail = $capacity;
         $this->head = 0;
         $this->size = 0;
+        $this->arr = array();
     }
 
     public function empty(): bool
@@ -30,5 +33,14 @@ class RingBuffer
     public function size(): int
     {
         return $this->size;
+    }
+
+    public function push(int $item): void
+    {
+        if ($this->size() + 1 <= $this->capacity()) {
+            $this->arr[] = $item;
+            $this->head++;
+            $this->size++;
+        }
     }
 }
