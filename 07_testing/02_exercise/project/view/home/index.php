769aeb72d9f58e6f6799686af7282eee65f25f67
lewi9 <lewinski@student.agh.edu.pl>
2022-11-21 22:37:24 +0100

    Style fixes

--- 07_testing/02_exercise/project/view/home/index.php
+++ 07_testing/02_exercise/project/view/home/index.php
@@ -7,10 +7,10 @@ if (isset($flags)) {
     echo '<ul>';
     foreach ($flags as $flag) {
         if ($flag instanceof \Model\Flagi) {
-            if($flag->id() == 1){
+            if ($flag->id() == 1) {
                 echo "<li class='error'>Provided token is invalid!</li>";
             }
-            if($flag->id() == 3){
+            if ($flag->id() == 3) {
                 echo "<li class='info'>Email successfully confirmed!</li>";
             }
             if ($flag->id() == 2) {
