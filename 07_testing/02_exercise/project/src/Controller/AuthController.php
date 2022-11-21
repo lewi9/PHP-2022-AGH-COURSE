1eedf0eba834dcf5178d65764140262e0f96cb20
lewi9 <lewinski@student.agh.edu.pl>
2022-11-21 03:53:38 +0100

    Restart

--- 07_testing/02_exercise/project/src/Controller/AuthController.php
+++ 07_testing/02_exercise/project/src/Controller/AuthController.php
@@ -8,11 +8,7 @@ use Storage\Exception\StorageException;
 
 class AuthController extends Controller
 {
-    /**
-     * @var array|int[]
-     */
-    private array $flags;
-    private int $validated;
+
     public function index(): Result
     {
         return view('auth.index')->withTitle("Auth");
@@ -20,9 +16,6 @@ class AuthController extends Controller
 
     public function register(): Result
     {
-        if ($this->validated) {
-            return view('auth.register')->withTitle("Register")->with('flags', $this->flags);
-        }
         return view('auth.register')->withTitle("Register");
     }
 
@@ -36,36 +29,12 @@ class AuthController extends Controller
         $storage->store($user);
     }
 
-    private function validate(): void
-    {
-        $this->flags = array(0, 0, 0, 0, 0, 0, 0, 0);
-        $names = array("id", "name", "surname", "email", "password", "password_confirmation");
-        for ($i=0; $i<6; ++$i) {
-            if (!$_POST[$names[$i]]) {
-                $this->flags[$i] = 1;
-                $this->flags[7] = 1;
-            }
-        }
-
-        if (!$this->flags[4] and !$this->flags[5]) {
-            if ($_POST[$names[4]] != $_POST[$names[5]]) {
-                $this->flags[7] = 1;
-                $this->flags[6] = 1;
-            }
-        }
-        $this->validated = 1;
-    }
-
     /**
      * @throws StorageException
      * @throws ExceptionAlias
      */
     public function confirmation_notice(): Result
     {
-        $this->validate();
-        if ($this->flags[7]) {
-            return redirect("/auth/register");
-        }
         $this->save_user("sqlite", new User((int) $_POST["id"], $_POST["name"], $_POST["surname"], $_POST["email"], $_POST["password"]));
         $this->save_user("mysql", new User((int) $_POST["id"], $_POST["name"], $_POST["surname"], $_POST["email"], $_POST["password"]));
         return view('auth.confirmation_notice')->withTitle("Confirmation notice");
