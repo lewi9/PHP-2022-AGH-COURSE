321adfb78d63b3a3155c8694c1883e43cc17e6cb
lewi9 <lewinski@student.agh.edu.pl>
2022-11-21 21:09:50 +0100

    connect Flagi

--- 07_testing/02_exercise/project/src/Model/Flag.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Model;
-
-class Flag extends Model
-{
-
-}
---
+++ 07_testing/02_exercise/project/src/Model/Flagi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Model;
+
+class Flag extends Model
+{
+    public function __construct(int $id)
+    {
+        parent::__construct($id);
+    }
+}
