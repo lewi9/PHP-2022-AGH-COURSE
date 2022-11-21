4ef46904dced594e63cae6c85bb7e5916d257bd6
lewi9 <lewinski@student.agh.edu.pl>
2022-11-21 06:31:27 +0100

    two constructors in user

--- 07_testing/02_exercise/project/src/Model/Model.php
+++ 07_testing/02_exercise/project/src/Model/Model.php
@@ -6,16 +6,5 @@ use Concept\Distinguishable;
 
 abstract class Model extends Distinguishable
 {
-    public string $name;
-    public string $surname;
-    public string $email;
-    public string $password;
 
-    public function __construct(int $id, string $name, string $surname, string $email)
-    {
-        parent::__construct($id);
-        $this->name = $name;
-        $this->surname = $surname;
-        $this->email = $email;
-    }
 }
--- 07_testing/02_exercise/project/src/Model/User.php
+++ 07_testing/02_exercise/project/src/Model/User.php
@@ -8,15 +8,26 @@ class User extends Model
 {
     public bool $confirmed;
     public string $token;
+    public string $name;
+    public string $surname;
+    public string $email;
+    public string $password;
 
     /**
      * @throws ExceptionAlias
      */
+    public function __construct1(int $id){
+        parent::__construct($id);
+    }
+
     public function __construct(int $id, string $name, string $surname, string $email, string $password)
     {
-        parent::__construct($id, $name, $surname, $email);
+        parent::__construct($id);
         $this->password = password_hash($password, PASSWORD_DEFAULT);
         $this->confirmed = false;
         $this->token = bin2hex(random_bytes(16));
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->email = $email;
     }
 }
