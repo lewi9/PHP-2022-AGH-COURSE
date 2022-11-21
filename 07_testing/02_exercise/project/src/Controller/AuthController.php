75396cf96288060991fcb00706aadeed75c91724
lewi9 <lewinski@student.agh.edu.pl>
2022-11-21 05:36:42 +0100

    AuthController something more

--- 07_testing/02_exercise/project/src/Controller/AuthController.php
+++ 07_testing/02_exercise/project/src/Controller/AuthController.php
@@ -19,6 +19,12 @@ class AuthController extends Controller
      */
     public function register(): Result
     {
+        if(isset($_POST["id"])) {
+            if(!(!$_POST["id"] or !$_POST["name"] or !$_POST["surname"] or !$_POST["email"] or !$_POST["password"] or !$_POST["password_confirmation"] or $_POST["password"] != $_POST["password_confirmation"])) {
+                return redirect("/auth/register");
+            }
+        }
+
         return view('auth.register')->withTitle("Register");
     }
 
@@ -38,11 +44,9 @@ class AuthController extends Controller
      */
     public function confirmation_notice(): Result
     {
-
         $user = new User((int) $_POST["id"], $_POST["name"], $_POST["surname"], $_POST["email"], $_POST["password"]);
         $this->save_model("sqlite", $user);
         $this->save_model("mysql", $user);
-
         return view('auth.confirmation_notice')->withTitle("Confirmation notice");
     }
 }
--- 07_testing/02_exercise/project/view/auth/register.php
+++ 07_testing/02_exercise/project/view/auth/register.php
@@ -1,43 +1,50 @@
 <h2>Register</h2>
 
 <?php
-$names = array("id", "name", "surname", "email", "password", "password_confirmation");
-$flag = 0;
-$flags = array(0,0,0,0,0,0,0);
-for ($i = 0; $i<6; $i++) {
-    if (!$_POST[$names[$i]]) {
-        $flag = 1;
-        $flags[$i] = 1;
+if(isset($_POST["id"])){
+    $names = array("id", "name", "surname", "email", "password", "password_confirmation");
+    $flag = 0;
+    $flags = array(0,0,0,0,0,0,0);
+    for ($i = 0; $i<6; $i++) {
+        if (!$_POST[$names[$i]]) {
+            $flag = 1;
+            $flags[$i] = 1;
+        }
     }
-}
-if (!($flags[4] or $flags[5])) {
-    if ($flags[4] != $flags[5]) {
-        $flags[6] = 1;
+    if (!($flags[4] or $flags[5])) {
+        if ($_POST[$names[4]] != $_POST[$names[5]]) {
+            $flags[6] = 1;
+        }
     }
-}
 
-if($flag){
-    echo "<ul>";
-    for($i = 0; $i<6; ++$i){
-        
+    if ($flag) {
+        echo "<ul>";
+        for ($i = 0; $i<6; ++$i) {
+            echo sprintf("<li class='error'>The %s filed cannot be empty</li>", $names[$i]);
+        }
+        if($flags[6]) {
+            echo "<li class='error'>The password confirmation filed does not match the password field</li>";
+        }
+        echo "</ul>";
     }
+
 }
 
 ?>
 
-<form method="post" action="/auth/confirmation_notice">
+<form method="post">
     <label for="id">Id:</label>
-    <input type="text" id="id" name="id" value="<?=$_POST["id"]?>"><br><br>
+    <input type="text" id="id" name="id" value="<?=$_POST["id"]?? ''?>"><br><br>
     <label for="name">Name:</label>
-    <input type="text" id="name" name="name" value="<?=$_POST["name"]?>>"<br><br>
+    <input type="text" id="name" name="name" value="<?=$_POST["name"]?? ''?>"><br><br>
     <label for="surname">Surname:</label>
-    <input type="text" id="surname" name="surname" value="<?=$_POST["surname"]?>"><br><br>
+    <input type="text" id="surname" name="surname" value="<?=$_POST["surname"]?? ''?>"><br><br>
     <label for="email">Email:</label>
-    <input type="text" id="email" name="email" value="<?=$_POST["email"]?>"><br><br>
+    <input type="text" id="email" name="email" value="<?=$_POST["email"]?? ''?>"><br><br>
     <label for="password">Password:</label>
-    <input type="text" id="password" name="password" value="<?=$_POST["password"]?>"><br><br>
+    <input type="text" id="password" name="password" value="<?=$_POST["password"]?? ''?>"><br><br>
     <label for="password_confirmation">Password confirmation:</label>
-    <input type="text" id="password_confirmation" name=password_confirmation value="<?=$_POST["password_confirmation"]?>"><br><br>
+    <input type="text" id="password_confirmation" name=password_confirmation value="<?=$_POST["password_confirmation"]?? ''?>"><br><br>
     <input type="submit" value="Create">
 
 </form>
