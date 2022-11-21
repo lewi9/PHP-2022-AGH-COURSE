8a16b0d58f6756d4cef5d22375cb2c737da20dfa
lewi9 <lewinski@student.agh.edu.pl>
2022-11-21 20:37:34 +0100

    login.html

--- 07_testing/02_exercise/project/view/auth/login.php
+++ 07_testing/02_exercise/project/view/auth/login.php
@@ -1,1 +1,9 @@
-<?php
+<h2>Login</h2>
+
+<form method="post">
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email"><br><br>
+    <label for="password">Password:</label>
+    <input type="text" id="password" name="password"><br><br>
+    <input type="submit" value="Enter">
+</form>
