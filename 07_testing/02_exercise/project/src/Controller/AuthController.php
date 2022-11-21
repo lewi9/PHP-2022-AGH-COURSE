b4248816afcaf1ed52fcfef9bcee7720815d11c4
lewi9 <lewinski@student.agh.edu.pl>
2022-11-21 03:42:56 +0100

    Validated flag

--- 07_testing/02_exercise/project/src/Controller/AuthController.php
+++ 07_testing/02_exercise/project/src/Controller/AuthController.php
@@ -8,6 +8,8 @@ use Storage\Exception\StorageException;
 
 class AuthController extends Controller
 {
+    private array $flags = array(0, 0, 0, 0, 0, 0, 0, 0);
+    private int $validated;
     public function index(): Result
     {
         return view('auth.index')->withTitle("Auth");
@@ -15,7 +17,10 @@ class AuthController extends Controller
 
     public function register(): Result
     {
-        return view('auth.register')->withTitle("Register");
+        if($validated) {
+            return
+        }
+        return view('auth.register')->withTitle("Register")->;
     }
 
     /**
@@ -28,28 +33,24 @@ class AuthController extends Controller
         $storage->store($user);
     }
 
-    /**
-     * @return int[]
-     */
-    private function validate(): array
+    private function validate(): void
     {
-        $flags = array(0, 0, 0, 0, 0, 0, 0, 0);
+        $this->flags = array(0, 0, 0, 0, 0, 0, 0, 0);
         $names = array("id", "name", "surname", "email", "password", "password_confirmation");
         for ($i=0; $i<6; ++$i) {
-            if (!isset($_POST[$names[$i]])) {
-                $flags[$i] = 1;
-                $flags[7] = 1;
+            if (!$_POST[$names[$i]]) {
+                $this->flags[$i] = 1;
+                $this->flags[7] = 1;
             }
         }
 
-        if (!$flags[4] and !$flags[5]) {
+        if (!$this->flags[4] and !$this->flags[5]) {
             if ($_POST[$names[4]] != $_POST[$names[5]]) {
-                $flags[7] = 1;
-                $flags[6] = 1;
+                $this->flags[7] = 1;
+                $this->flags[6] = 1;
             }
         }
-
-        return $flags;
+        $this->validated = 1;
     }
 
     /**
@@ -58,12 +59,12 @@ class AuthController extends Controller
      */
     public function confirmation_notice(): Result
     {
-        $flags = $this->validate();
-        if ($flags[7]) {
-            return view('auth.register')->withTitle("Register")->withLocation("/auth/register")->with('flags', $flags);
+        $this->validate();
+        if ($this->flags[7]) {
+            return redirect("/auth/register");
         }
         $this->save_user("sqlite", new User((int) $_POST["id"], $_POST["name"], $_POST["surname"], $_POST["email"], $_POST["password"]));
         $this->save_user("mysql", new User((int) $_POST["id"], $_POST["name"], $_POST["surname"], $_POST["email"], $_POST["password"]));
-        return view('auth.confirmation_notice')->withTitle("Confirmation notice")->with('flags', $flags);
+        return view('auth.confirmation_notice')->withTitle("Confirmation notice");
     }
 }
--- 07_testing/02_exercise/project/view/auth/register.php
+++ 07_testing/02_exercise/project/view/auth/register.php
@@ -17,10 +17,11 @@
 </form>
 
 <?php
+echo isset($flags);
 if (isset($flags)) {
     $names = array("id", "name", "surname", "email", "password", "password_confirmation");
     echo "<ul>";
-    for ($i=0;$i<6;++$i) {
+    for ($i = 0; $i < 6; ++$i) {
         if ($flags[$i]) {
             echo "<li class='error'>" . "The " . $names[$i] . " filed cannot be empty" . "</li>";
         }
