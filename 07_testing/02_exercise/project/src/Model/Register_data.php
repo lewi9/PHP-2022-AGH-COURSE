54cafbbae251a8d36251b63930bf4c4b35f93cb3
lewi9 <lewinski@student.agh.edu.pl>
2022-11-21 04:21:49 +0100

    Register_data

--- 07_testing/02_exercise/project/src/Model/Register_data.php
+++ 07_testing/02_exercise/project/src/Model/Register_data.php
@@ -4,13 +4,18 @@ namespace Model;
 
 class Register_data extends Model
 {
+    public string $password_confirmed;
+    public int $was_id = 1;
 
-    private string $password_confirmed;
-    public function __construct(int $id, string $name, string $surname, string $email, string $password, string $password_confirmed)
+    public function __construct(string $id, string $name, string $surname, string $email, string $password, string $password_confirmed)
     {
+        if (!$id) {
+            $this->was_id = 0;
+            $id = 1;
+        }
+        $id = (int) $id;
         parent::__construct($id, $name, $surname, $email);
         $this->password = $password;
         $this->password_confirmed = $password_confirmed;
-
     }
 }
