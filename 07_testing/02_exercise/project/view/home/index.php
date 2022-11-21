1b882b11081c8f6994f4ee1918c50e4cb684fc29
lewi9 <lewinski@student.agh.edu.pl>
2022-11-21 23:10:33 +0100

    Delete one for in index home

--- 07_testing/02_exercise/project/view/home/index.php
+++ 07_testing/02_exercise/project/view/home/index.php
@@ -7,28 +7,28 @@ if (isset($flags)) {
         if ($flag instanceof \Model\Flagi) {
             if ($flag->id() == 1) {
                 echo "<li class='error'>Provided token is invalid!</li>";
+                continue;
             }
             if ($flag->id() == 3) {
                 echo "<li class='info'>Email successfully confirmed!</li>";
+                continue;
             }
             if ($flag->id() == 2) {
                 echo "<li class='error'>Email $flag->email does not exist!</li>";
+                continue;
             }
 
-        }
-    }
-    echo '<ul>';
-    
-    foreach($flags as $flag) {
-        if ($flag->id() == 4) {
-            echo "Welcome back " . $flag->name . "!";
-            echo "<h3 class='user'>$flag->name $flag->surname</h3>";
-            echo "<a href='/auth/logout'>Logout</a>";
-        } else {
-            echo "<a href='/auth/login>Login</a>";
-            echo "<a href='/auth/register'>Register</a>";
-        }
-    }
+            echo '<ul>';
 
+            if ($flag->id() == 4) {
+                echo "Welcome back " . $flag->name . "!";
+                echo "<h3 class='user'>$flag->name $flag->surname</h3>";
+                echo "<a href='/auth/logout'>Logout</a>";
+            } else {
+                echo "<a href='/auth/login>Login</a>";
+                echo "<a href='/auth/register'>Register</a>";
+            }
+        }
+    }
 
 }
