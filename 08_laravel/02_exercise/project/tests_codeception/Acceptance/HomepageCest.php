04bb18c89280f9bffe719033fa30769b99842079
lewi9 <lewinski@student.agh.edu.pl>
2022-11-30 20:13:08 +0100

    Finished tutorial.

---
+++ 08_laravel/02_exercise/project/tests_codeception/Acceptance/HomepageCest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class HomepageCest
+{
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->wantTo('see Laravel links on homepage');
+
+        $I->amOnPage('/');
+
+        $I->seeInTitle('Laravel');
+
+        $I->seeLink("Documentation", "https://laravel.com/docs");
+        $I->seeLink("Laracasts", "https://laracasts.com");
+        $I->seeLink("Forge", "https://forge.laravel.com");
+        
+        $I->dontSeeInDatabase('users');
+    }
+}
