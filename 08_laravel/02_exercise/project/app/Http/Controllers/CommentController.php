e0aa68313ad4495c2dc9797165def280b1b3f29b
lewi9 <lewinski@student.agh.edu.pl>
2022-12-02 17:18:00 +0100

    Comment method in CommentsController.

--- 08_laravel/02_exercise/project/app/Http/Controllers/CommentController.php
+++ 08_laravel/02_exercise/project/app/Http/Controllers/CommentController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     public function index()
     {
-        $comments = \App\Models\Comment::findOrFail(1);
+        $comments = Comment::findOrFail(1);
         return view('index')->with('comments', $comments);
+    }
+
+    public function comment($title)
+    {
+        $comment = Comment::where('title', $title)->first();
+        return view('index')->with('comment', $comment);
     }
 }
