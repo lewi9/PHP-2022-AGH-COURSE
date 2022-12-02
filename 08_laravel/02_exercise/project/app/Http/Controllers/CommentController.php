fca8743485b3719f40921a184451b1498be55f34
lewi9 <lewinski@student.agh.edu.pl>
2022-12-02 17:07:12 +0100

    Index in Comments controller.

--- 08_laravel/02_exercise/project/app/Http/Controllers/CommentController.php
+++ 08_laravel/02_exercise/project/app/Http/Controllers/CommentController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function index()
+    {
+        $comments = \App\Models\Comment::findOrFail(1);
+        return view('index')->with('comments', $comments);
+    }
 }
--- 08_laravel/02_exercise/project/database/migrations/2022_12_02_152821_create_comments_table.php
+++ 08_laravel/02_exercise/project/database/migrations/2022_12_02_152821_create_comments_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('text');
             $table->timestamps();
         });
     }
