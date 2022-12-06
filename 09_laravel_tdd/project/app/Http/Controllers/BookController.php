cfcb96071ec38acae1e9bc044faf9a2b7b16566d
lewi9 <lewinski@student.agh.edu.pl>
2022-12-06 14:12:17 +0100

    Store method.

--- 09_laravel_tdd/project/app/Http/Controllers/BookController.php
+++ 09_laravel_tdd/project/app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -16,5 +17,21 @@ class BookController extends Controller
     public function create(): View
     {
         return view('books.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'isbn' => ['required', 'string', 'min:13', 'max:13', 'unique:' . Book::class],
+            'title' => ['required', 'string'],
+            'description' => ['required', 'string'],
+        ]);
+        Book::create([
+            'isbn' => $request->isbn,
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+
+        return redirect("books.show.$request->id");
     }
 }
