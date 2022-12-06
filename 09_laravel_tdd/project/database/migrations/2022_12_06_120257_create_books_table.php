a83d36acd729c32d5f2e9e3691736155dc6aaf20
lewi9 <lewinski@student.agh.edu.pl>
2022-12-06 13:24:36 +0100

    Migration file.

--- 09_laravel_tdd/project/database/migrations/2022_12_06_120257_create_books_table.php
+++ 09_laravel_tdd/project/database/migrations/2022_12_06_120257_create_books_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('isbn');
+            $table->string('description');
+            $table->string('title');
             $table->timestamps();
         });
     }
