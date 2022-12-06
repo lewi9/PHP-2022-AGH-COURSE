43a9feb906b0f352a82af7bdd6d7df904340966b
lewi9 <lewinski@student.agh.edu.pl>
2022-12-06 13:19:23 +0100

    Book view.

--- 09_laravel_tdd/project/app/Http/Controllers/BookController.php
+++ 09_laravel_tdd/project/app/Http/Controllers/BookController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class BookController extends Controller
 {
-    public function index():
+    public function index(): View
     {
-        return view('books.index')->with('books')
+        return view('books.index')->with('books', Book::all());
     }
 }
---
+++ 09_laravel_tdd/project/resources/views/books/index.blade.php
@@ -0,0 +1,27 @@
+<x-guest-layout>
+    {{-- adapted from resources/views/components/auth-card.blade.php --}}
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div>
+            <h2>List of books</h2>
+        </div>
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{-- created based on https://flowbite.com/docs/typography/lists/ --}}
+            <dl class="max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
+                @if (count($books) === 0)
+                    No books in database.
+                @else
+                    @foreach($books as $book)
+                        <div class="flex flex-col pb-3">
+                            <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">
+                                <a href="{{ route('comments.show', $book) }}">{{ $book->title }}</a>
+                            </dt>
+                            <dd class="text-lg font-semibold">
+                                @markdown($book->text)
+                            </dd>
+                        </div>
+                    @endforeach
+                @endif
+            </dl>
+        </div>
+    </div>
+</x-guest-layout>
